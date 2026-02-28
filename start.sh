ffe97b1646be52737e77f9ffe61be09b34f0a331
Jackson147 <jaswahhihi@gmail.com>
2026-02-28 16:30:55 +0000

    Improved the startup script

--- start.sh
+++ start.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+
+# 1. Setup
 mkdir -p ./downloads
 xhost +local:docker
+
+# 2. Define the cleanup function
+cleanup() {
+    echo " Catching SIGINT (Ctrl+C)... shutting down."
+    docker compose down
+    exit 0
+}
+
+# 3. Register the trap
+# This tells the script: "If you see SIGINT (Ctrl+C), run the cleanup function"
+trap cleanup SIGINT
+
+# 4. Start Docker Compose
+# We use 'up' normally. If you hit Ctrl+C, the trap triggers.
 docker compose up
